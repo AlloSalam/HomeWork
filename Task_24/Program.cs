cf0677128ffa0af44d530c9c4483a33e5a7c1a6a
Kirill Reznikov <98495087+AlloSalam@users.noreply.github.com>
2022-02-14 20:53:31 +0400

    Add files via upload

---
+++ Task_24/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+System.Console.Write("Введите число:   ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] GetCubeTable(int N)
+{
+    int[] cubs = new int[N];
+    int number = 1;
+    for (int i = 0; i < N; i++)
+    {
+        cubs[i] = number * number * number;
+        number++;
+    }
+    return cubs;
+}
+
+void PrintCubs(int[] array)
+{
+    int number = 1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{number} ^ 3 = ");
+        System.Console.WriteLine(array[i]);
+        number++;
+    }
+}
+
+int[] CubeTable = GetCubeTable(N);
+PrintCubs(CubeTable);
