903d7aa85c7b7ee5b83ecfd83fa368f15a84f63b
Kirill Reznikov <98495087+AlloSalam@users.noreply.github.com>
2022-02-14 20:41:17 +0400

    Add files via upload

---
+++ Task_20/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+int chetNum = new Random().Next(1, 5);
+string diap = "";
+
+if (chetNum == 1) diap = "x > 0, y > 0";
+else if (chetNum == 2) diap = "x < 0, y > ";
+else if (chetNum == 3) diap = "x < 0, y < 0";
+else diap = "x > 0, y < 0";
+
+System.Console.WriteLine($"Для {chetNum}-й четверти координаты точки должны быть заданы в диапазоне ( {diap} )");
+System.Console.WriteLine();
