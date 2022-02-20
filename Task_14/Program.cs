0702521884e4354e32fb1ddd4b0fe8b571250215
Kirill Reznikov <98495087+AlloSalam@users.noreply.github.com>
2022-02-21 01:58:51 +0400

    Add files via upload

---
+++ Task_14/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+int Num = new Random().Next(1, 1500000);
+Console.WriteLine($"Выпало рандомное число {Num}");
+
+if (Num / 100 > 0)
+    {
+        int newNum = int.Parse(Convert.ToString(Num)[2].ToString());
+        Console.WriteLine($"{newNum} является третьей цифрой числа");
+    }
+else
+    {
+        Console.WriteLine("У числа нет третьей цифры");
+    }
