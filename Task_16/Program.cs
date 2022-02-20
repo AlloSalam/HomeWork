e008c012416c7a12db30b6b2bfffdfff73c76d28
Kirill Reznikov <98495087+AlloSalam@users.noreply.github.com>
2022-02-21 01:59:24 +0400

    Add files via upload

---
+++ Task_16/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+int Day = new Random().Next(1, 8);
+string[] Days = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
+string Result = "";
+
+if (Day > 5) 
+    {
+        Result = ", выходной день";
+    }
+else 
+    {
+        Result = ", рабочий день";
+    }
+
+Console.WriteLine($"{Day}-й день недели - это {Days[Day - 1]}{Result}");
