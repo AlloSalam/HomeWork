0aaf49f5706087563ec92c4898ffb42fd32cc655
Kirill Reznikov <98495087+AlloSalam@users.noreply.github.com>
2022-02-21 01:58:22 +0400

    Add files via upload

---
+++ Task_12/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+int Num = new Random().Next(100, 1000);
+int NewNum = 0;
+NewNum = Num / 100 * 10 + Num % 10;
+
+Console.WriteLine($"После удаления второй цифры из числа {Num}, получилось новое число {NewNum}");
