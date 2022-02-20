a8049c024c05f8d5b857d7ead93fd96f8fc5536f
Kirill Reznikov <98495087+AlloSalam@users.noreply.github.com>
2022-02-21 01:57:49 +0400

    Add files via upload

---
+++ Task_10/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+int secnum = new Random().Next(100, 1000);
+Console.WriteLine($"Выпало рандомное число {secnum}");
+Console.WriteLine($"Вторая цифра числа - {secnum / 10 % 10}");
+ 
