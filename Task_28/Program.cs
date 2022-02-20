8e0ed75c0438bc30547fc3fd52059afcc668fc37
Kirill Reznikov <98495087+AlloSalam@users.noreply.github.com>
2022-02-21 02:51:46 +0400

    Add files via upload

---
+++ Task_28/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+int num = new Random().Next(1, 100000);
+int sum = 0;
+Console.Write($"Сумма цифр числа {num} равна ");
+
+while (num > 0)
+{
+    int digit = num % 10;
+    sum = sum + digit;
+    num = num / 10;
+}
+
+Console.WriteLine(sum);
