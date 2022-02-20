b88cda94cac0e80bd215a16f2b143b0b8251167f
Kirill Reznikov <98495087+AlloSalam@users.noreply.github.com>
2022-02-21 02:51:12 +0400

    Add files via upload

---
+++ Task_26/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+Console.WriteLine("Впишите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Впишите степень");
+int exponent = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int result = num;
+
+while (count < exponent)
+{
+    result = result * num;
+    count++;
+}
+
+Console.WriteLine($"{num} в {exponent} степени = {result}");
