31feabcad2e3bb121fc98363e7239ba005416307
Kirill Reznikov <98495087+AlloSalam@users.noreply.github.com>
2022-02-24 17:30:06 +0400

    Add files via upload

---
+++ Task_44/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+int Crossing(int k1, int k2, int b1, int b2)
+{
+    int x, y;
+    x = (b2 - b1)/(k1-k2);
+    y = k1*x + b1;
+    Console.Write($"\tЭти 2 прямые пересекаются в точке {x}, "); 
+    return y;
+}
+Console.WriteLine(Crossing(1,2,3,4));
