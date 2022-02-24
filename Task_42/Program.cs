d314d09f768f11eac71c80a85be13b55100c7b0f
Kirill Reznikov <98495087+AlloSalam@users.noreply.github.com>
2022-02-24 17:28:05 +0400

    Add files via upload

---
+++ Task_42/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.Write("Введите кол-во чисел: ");    
+int volume = Convert.ToInt32(Console.ReadLine());   //Здесь выбираем из скольки чисел нужно найти числа больше 0
+int count = 0;
+
+while (volume > 0)
+{
+    Console.Write("Введите число:   ");
+    int number = Convert.ToInt32(Console.ReadLine());      //Здесь вписываем числа
+    if (number > 0) count++;
+    volume--;
+}
+Console.WriteLine($"Количество чисел больше 0: {count}");
