b5c1f1219a92a1dcc46f6a7e5eb7614e9deadf8e
Kirill Reznikov <98495087+AlloSalam@users.noreply.github.com>
2022-03-03 20:52:05 +0400

    Add files via upload

---
+++ Task_63/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+int[,,] array = new int[4, 4, 4];
+
+void FirstArray(int[,,] array)
+{
+    int number = 10;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int l = 0; l < array.GetLength(2); l++)
+            {
+                array[i, j, l] = number;
+                number++;
+            }
+        }
+    }
+}
+
+void WriteArray(int[,,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int l = 0; l < array.GetLength(2); l++)
+            {
+                Console.WriteLine($"Индексам [{i}-{j}-{l}] присвоено значение {array[i, j, l]} ");
+
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+FirstArray(array);
+WriteArray(array);
